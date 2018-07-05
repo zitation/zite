5db15b409f4662a67743e9c772ef0de8bbe891f6
Liam Kinne <kinneliam@gmail.com>
2018-07-05 22:22:16 +1000

    clean up

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
 
   app.post('/meta', (req, res) => {
     const url = req.body.url
-    console.log(url)
+
     if (url) {
       if (url === 'test') {
         var response = {}
@@ -19,27 +19,26 @@ module.exports = function (app) {
       }
       else {
         urlMetadata(url, {timeout: 5000}).then(
-          function (meta) { // success handler
-            console.log(meta)
-            try {
-              var response = {
-                title: meta['title'] || meta['og:title'],
-                author: meta['author'] || meta['article:author'] || meta['og:article:author'],
-                date_published: meta['article:published_time'] || meta['og:article:published_time']
-              }
-
-              if (response.date_published)
-                response.date_published = response.date_published.substring(0, 10)
-
-              res.json(response)
+        function (meta) { // success handler
+          try {
+            var response = {
+              title: meta['title'] || meta['og:title'],
+              author: meta['author'] || meta['article:author'] || meta['og:article:author'],
+              date_published: meta['article:published_time'] || meta['og:article:published_time']
             }
-            catch (e) {
-              console.log(e)
-            }
-          },
-          function (error) { // failure handler
-            console.log(error)
-          })
+
+            if (response.date_published)
+              response.date_published = response.date_published.substring(0, 10)
+
+            res.json(response)
+          }
+          catch (e) {
+            console.log(e)
+          }
+        },
+        function (error) { // failure handler
+          console.log(error)
+        })
       }
     }
   })
