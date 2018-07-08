536223a983f434767f90e57cb6f9b60f5f4fe4ed
Liam Kinne <kinneliam@gmail.com>
2018-07-08 19:28:13 +1000

    fix selector style

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -22,10 +22,11 @@ select {
   text-align: center;
   color: #fcf7ff;
   background-color: rgba(0, 0, 0, 0);
-  border: 0.5em solid #fcf7ff;
+  border: 0.35em solid #fcf7ff;
   margin: 0 0.25em 1em;
   padding: 0.5em 1em;
   border-radius: 2em;
+  box-shadow: 0.25em 0.25em 0.5em rgba(0, 0, 0, 0.125);
   /* remove default arrow on dorpdown select */
   -webkit-appearance: none;
   -moz-appearance: none;
