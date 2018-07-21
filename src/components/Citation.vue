331d6aa863cd8a44235855a53efe7788b9c94ea3
Liam Kinne <kinneliam@gmail.com>
2018-07-22 07:33:23 +1000

    remove http/https prefix when rendering

--- src/components/Citation.vue
+++ src/components/Citation.vue
@@ -9,6 +9,13 @@ export default {
   },
   methods: {
     template (data) {
+      // remove http/https
+      if (data.url.startsWith('https://')) {
+        data.url = data.url.slice(8)
+      } else if (data.url.startsWith('http://')) {
+        data.url = data.url.slice(7)
+      }
+
       return `${data.author}. ${data.date_published}. ${data.title}. Retrieved ${data.date_accessed}, from ${data.url}`
     }
   }
