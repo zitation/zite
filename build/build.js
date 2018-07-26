2ca9210f4d25d119ae778d940453cf31ab9b9c07
Liam Kinne <kinneliam@gmail.com>
2018-07-26 21:33:02 +1000

    make deps production

--- build/build.js
+++ build/build.js
@@ -1,19 +1,19 @@
 'use strict'
 
-// require('./check-versions')()
+require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
 
-//const ora = require('ora')
+const ora = require('ora')
 const rm = require('rimraf')
 const path = require('path')
-//const chalk = require('chalk')
+const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
 
-//const spinner = ora('building for production...')
-//spinner.start()
+const spinner = ora('building for production...')
+spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
@@ -28,10 +28,10 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       chunkModules: false
     }) + '\n\n')
 
-    //console.log(chalk.cyan('  Build complete.\n'))
-    //console.log(chalk.yellow(
-    //  '  Tip: built files are meant to be served over an HTTP server.\n' +
-    //  '  Opening index.html over file:// won\'t work.\n'
-    //))
+    console.log(chalk.cyan('  Build complete.\n'))
+    console.log(chalk.yellow(
+      '  Tip: built files are meant to be served over an HTTP server.\n' +
+      '  Opening index.html over file:// won\'t work.\n'
+    ))
   })
 })
