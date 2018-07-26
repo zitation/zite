b6f341b6cc1b48c4fa08eafc3f3602c52df2eb8c
Liam Kinne <kinneliam@gmail.com>
2018-07-26 21:26:25 +1000

    comment out ora

--- build/build.js
+++ build/build.js
@@ -4,7 +4,7 @@
 
 process.env.NODE_ENV = 'production'
 
-const ora = require('ora')
+//const ora = require('ora')
 const rm = require('rimraf')
 const path = require('path')
 //const chalk = require('chalk')
@@ -12,13 +12,13 @@ const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
 
-const spinner = ora('building for production...')
-spinner.start()
+//const spinner = ora('building for production...')
+//spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
   webpack(webpackConfig, function (err, stats) {
-    spinner.stop()
+    //spinner.stop()
     if (err) throw err
     process.stdout.write(stats.toString({
       colors: true,
