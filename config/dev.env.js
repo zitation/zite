c643937869d0ed80b71c6374b5ea8c84e015cd56
Liam Kinne <kinneliam@gmail.com>
2018-07-29 17:35:03 +1000

    quotation marks are needed

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: 'development',
-  ROOT_API: 'http://localhost:8080'
+  NODE_ENV: '"development"',
+  ROOT_API: '"http://localhost:8080"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
-  NODE_ENV: 'production',
-  ROOT_API: 'https://zite-citation.herokuapp.com/',
-  HEROKU_POSTGRESQL_CRIMSON_URL: 'postgres://ecswhwvurektvo:e2672f92925ef1e9ec4c55581329f7b20738f8310158e0575ed7cffe2fc64e2c@ec2-50-16-241-91.compute-1.amazonaws.com:5432/d9grffca0a313r'
+  NODE_ENV: '"production"',
+  ROOT_API: '"https://zite-citation.herokuapp.com/"',
+  DATABASE_URL: '"postgres://ecswhwvurektvo:e2672f92925ef1e9ec4c55581329f7b20738f8310158e0575ed7cffe2fc64e2c@ec2-50-16-241-91.compute-1.amazonaws.com:5432/d9grffca0a313r"'
 }
