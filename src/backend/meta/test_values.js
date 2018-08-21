32955cca720bd39268d287d1605f6f7957a3d104
Liam Kinne <kinneliam@gmail.com>
2018-08-21 12:39:59 +1000

    refactor meta test values

---
+++ src/backend/meta/test_values.js
@@ -0,0 +1,6 @@
+module.exports = {
+  title: 'Happy Little Trees',
+  author: 'Bob Ross',
+  date_accessed: '2018-01-01',
+  date_published: '1983-01-11',
+}
--- src/backend/server.js
+++ src/backend/server.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
   const bodyParser = require('body-parser')
   const cors = require('cors')
   const urlMetadata = require('url-metadata')
-  const meta_scrape = require('./meta_scrape.js')
+  const meta_scrape = require('./meta/scrape.js')
   app.use(bodyParser.json())
   app.use(cors())
 
@@ -11,12 +11,8 @@ module.exports = function (app) {
 
     if (url) {
       if (url === 'test') {
-        var response = {}
-        response.title = 'Happy Little Trees'
-        response.author = 'Bob Ross'
-        response.date_accessed = '2018-01-01'
-        response.date_published = '1983-01-11'
-        res.json(response)
+        var test_values = require('./meta/test_values.js')
+        res.json(test_values)
       }
       else {
         meta_scrape.get(url, res)
