e0118e420a26e9fc245350ea33be519fe6e80420
Liam Kinne <kinneliam@gmail.com>
2018-08-26 00:14:14 +1000

    shorten debounce

--- src/components/entry_forms/Website.vue
+++ src/components/entry_forms/Website.vue
@@ -31,7 +31,7 @@ export default {
   methods: {
     updateUrl: _.debounce(function () {
       this.getMeta()
-    }, 500),
+    }, 250),
     async getMeta () {
       var response = false
       try {
