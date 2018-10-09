c50f66011907a73d6db2de4c8de824d97ccbf69a
liamkinne <kinneliam@gmail.com>
2018-10-10 05:59:52 +1100

    fix accidentally mutating state without vuex

--- src/services/reference_composer/formats/apa.js
+++ src/services/reference_composer/formats/apa.js
@@ -13,16 +13,17 @@ export default {
   citation: {
     website: function (meta) {
       // remove http/https
+      let url = meta.url
       if (meta.url.startsWith('https://')) {
-        meta.url = meta.url.slice(8)
+        url = meta.url.slice(8)
       } else if (meta.url.startsWith('http://')) {
-        meta.url = meta.url.slice(7)
+        url = meta.url.slice(7)
       }
 
       var datePublished = getDateComponentsFromString(meta.date_published)
       var dateAccessed = getDateComponentsFromString(meta.date_accessed)
 
-      return `${meta.author[0].name}. (${datePublished.year}). ${meta.title}. Retrieved ${dateAccessed.monthName} ${dateAccessed.date}, ${dateAccessed.year}, from ${meta.url}`
+      return `${meta.author[0].name}. (${datePublished.year}). ${meta.title}. Retrieved ${dateAccessed.monthName} ${dateAccessed.date}, ${dateAccessed.year}, from ${url}`
     },
     book: function (meta) {
       var datePublished = getDateComponentsFromString(meta.date_published)
