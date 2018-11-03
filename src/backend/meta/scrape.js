7a5bdfb52eaad3036f6544d285873ee137cc2e73
liamkinne <kinneliam@gmail.com>
2018-11-04 07:00:11 +1100

    use arrow functions

--- src/backend/meta/scrape.js
+++ src/backend/meta/scrape.js
@@ -3,7 +3,7 @@ const urlMetadata = require('url-metadata')
 module.exports = {
   get: function (url, response) {
     urlMetadata(url).then(
-      function (meta) {
+      (meta) => {
         let date = new Date(meta['article:published_time'] || meta['og:article:published_time'])
 
         const bibjson = {
@@ -15,7 +15,7 @@ module.exports = {
 
         response.json(bibjson)
       },
-      function (error) {
+      (error) => {
         console.error(error)
       })
   }
