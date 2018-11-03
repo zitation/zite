fc4713970715f3d5e0b62574edad66deb8582aca
liamkinne <kinneliam@gmail.com>
2018-11-04 07:27:30 +1100

    clean up date extraction

--- src/backend/meta/scrape.js
+++ src/backend/meta/scrape.js
@@ -8,13 +8,11 @@ module.exports = {
     } else {
       urlMetadata(url).then(
         (meta) => {
-          let date = new Date(meta['article:published_time'] || meta['og:article:published_time'])
-
           const bibjson = {
             'type': meta['og:type'],
             'title': meta['title'] || meta['og:title'],
             'author': [{'name': meta['author'] || meta['article:author'] || meta['og:article:author']}],
-            'date': date
+            'date': new Date(meta['article:published_time'] || meta['og:article:published_time'])
           }
 
           response.json(bibjson)
